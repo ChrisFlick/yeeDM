155f97767e9b2059d87ff46bbdb7ced13c7c51bb
Camicdev <Interhacktive@gmail.com>
2022-10-06 15:16:13 -0700

    UI comes to user
    based on world locked distance

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class FaceCamera : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool isPhoneInterface = false;
+    public bool isAttachedToPhone = false;
+    public Vector3 initialLocation;
+
+
+    public Transform target;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+
+
     void Start()
     {
-        
+        initialLocation = transform.position;
+        target = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+
+        if(Vector3.Distance(transform.position, Camera.main.transform.position) < 1.5f)
+        {
+            isAttachedToPhone = true;
+        }
+        else
+        {
+            isAttachedToPhone = false;
+        }
+        if (isPhoneInterface)
+        {
+            if (!isAttachedToPhone)
+            {
+                transform.LookAt(Camera.main.transform);
+                transform.position = Vector3.SmoothDamp(transform.position, initialLocation, ref velocity, smoothTime);
+            }
+            else
+            {
+                Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, 0.6f));
+                transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+                //transform.localEulerAngles = new Vector3(0, 180, 0);
+                transform.LookAt(Camera.main.transform);
+            }
+        }
+        else
+        {
+            transform.LookAt(Camera.main.transform);
+        }
+        
+        
     }
 }
